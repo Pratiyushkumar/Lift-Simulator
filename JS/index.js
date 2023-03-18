02e651f8b41fa7c7174b021ef86119c73e07feec
Pratiyush Kumar <pratiyush.inv@gmail.com>
2023-03-18 16:57:21 +0530

    Updated commit

---
+++ JS/index.js
@@ -0,0 +1,8 @@
+const floorInput = document.querySelector(".floor__input");
+const liftInput = document.querySelector(".lift__input");
+const submitBtn = document.querySelector(".submit__btn");
+
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  
+});
